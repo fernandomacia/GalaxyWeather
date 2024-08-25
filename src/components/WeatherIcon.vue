1d4f0a464f314c4f03b4f932c4a0d99db9a71813
Fernando Maciá <fernando.macia@hotmail.es>
2024-08-25 12:13:43 +0200

    Weather Icon

---
+++ src/components/WeatherIcon.vue
@@ -0,0 +1,15 @@
+<template>
+    <img :src="iconUrl" alt="Weather Icon" />
+</template>
+  
+<script>
+    export default {
+        props: ['icon'],
+        computed: {
+            iconUrl() {
+                return `https://openweathermap.org/img/wn/${this.icon}@2x.png`;
+            },
+        },
+    };
+</script>
+  
